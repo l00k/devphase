e7ecd0102f3578c40ff8c02fdfc2b61978f50fb6
l00k <l00k+github@protonmail.com>
2022-10-12 18:28:01 +0200

    contract factory interface

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # devPHAse
 Development tool for Phala Phat contracts.
 
+<!--
 ![](https://img.shields.io/badge/Coverage-97%25-83A603.svg?prefix=$coverage$)
+-->
--- src/PhatContract.ts
+++ src/PhatContract.ts
@@ -2,33 +2,42 @@ import * as PhalaSdk from '@phala/sdk';
 import { ContractPromise } from '@polkadot/api-contract';
 import type * as Base from '@polkadot/api-contract/base/types';
 import { ContractCallOutcome, ContractOptions } from '@polkadot/api-contract/types';
-import type { ApiTypes } from '@polkadot/api/types';
 
 
-
-export interface ContractQuery<ApiType extends ApiTypes>
-    extends Base.ContractQuery<ApiType>
+export interface ContractQuery
+    extends Base.ContractQuery<'promise'>
 {
     (
         certificateDate : PhalaSdk.CertificateData,
         options : ContractOptions,
         ...params : unknown[]
-    ) : Base.ContractCallResult<ApiType, ContractCallOutcome>;
+    ) : Base.ContractCallResult<'promise', ContractCallOutcome>;
 }
 
-export interface MapMessageQuery<ApiType extends ApiTypes>
-    extends Base.MapMessageQuery<ApiType>
+export interface MapMessageQuery
+    extends Base.MapMessageQuery<'promise'>
 {
-    [message : string] : ContractQuery<ApiType>;
+    [message : string] : ContractQuery;
+}
+
+export interface ContractTx
+    extends Base.ContractTx<'promise'>
+{
+}
+
+export interface MapMessageTx
+    extends Base.MapMessageTx<'promise'>
+{
+    [message : string] : ContractTx;
 }
 
 export declare class PhatContract
     extends ContractPromise
 {
-
+    
     public readonly contractId : string;
     
-    public get query () : MapMessageQuery<'promise'>;
-    public get tx () : Base.MapMessageTx<'promise'>;
+    public get query () : MapMessageQuery;
+    public get tx () : MapMessageTx;
     
 }
