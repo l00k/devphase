21f4f79978913321e61a53eae59ed9a7abc69101
l00k <l00k+github@protonmail.com>
2022-11-01 13:51:20 +0100

    progress in type binding generation

--- src/service/type-binding/AbiTypeBindingProcessor.ts
+++ src/service/type-binding/AbiTypeBindingProcessor.ts
@@ -1,45 +1,24 @@
 import { ContractMetadata } from '@/def';
+import { BuiltType, StructTypeBuilder } from '@/service/type-binding/StructTypeBuilder';
 import { Exception } from '@/utils/Exception';
 import upperFirst from 'lodash/upperFirst';
 import path from 'path';
 import * as TsMorph from 'ts-morph';
 
 
-type Type = {
-    kind : string,
-    meta : ContractMetadata.TypeDefType,
-};
-
 export class AbiTypeBindingProcessor
 {
     
-    protected _types : Record<number, Type>;
+    protected _structTypeBuilder : StructTypeBuilder;
     
     
     public constructor (
         protected readonly _abi : ContractMetadata.ABI
     )
     {
-        this._buildInternalTypes();
+        this._structTypeBuilder = new StructTypeBuilder(this._abi.types);
     }
     
-    protected _buildInternalTypes ()
-    {
-        this._types = Object.fromEntries(
-            this._abi.types.map(typeDef => {
-                return [
-                    typeDef.id,
-                    {
-                        kind: Object.keys(typeDef.type.def)[0],
-                        meta: typeDef.type,
-                    }
-                ];
-            })
-        );
-        
-        
-        console.dir(this._types, { depth: 10 });
-    }
     
     public static async createTypeBindingFile (
         filePath : string,
@@ -71,6 +50,7 @@ export class AbiTypeBindingProcessor
             isExported: true,
             name: contractName,
             statements: [
+                ...context._structTypeBuilder.getTypeStatements(),
                 ...context.buildMapMessageQueryInterface(),
                 ...context.buildMapMessageTxInterface(),
                 ...context.buildContractClass(),
@@ -131,7 +111,7 @@ export class AbiTypeBindingProcessor
             .filter(message => message.mutates === false);
         for (const queryMessage of queryMessages) {
             const name = upperFirst(queryMessage.label);
-            const returnType = this.buildCodecType(queryMessage.returnType);
+            const returnType = this._structTypeBuilder.getNativeType(queryMessage.returnType.type);
             
             queriesModuleInterfaces.push({
                 isExported: true,
@@ -155,7 +135,7 @@ export class AbiTypeBindingProcessor
                             ...queryMessage.args.map<TsMorph.ParameterDeclarationStructure>(arg => ({
                                 kind: TsMorph.StructureKind.Parameter,
                                 name: arg.label,
-                                type: this.buildNativeType(arg.type),
+                                type: this._structTypeBuilder.getNativeType(arg.type.type),
                             })),
                         ],
                         returnType: `DPT.CallResult<DPT.CallOutcome<${returnType}>>`,
@@ -213,7 +193,7 @@ export class AbiTypeBindingProcessor
                             ...txMessage.args.map<TsMorph.ParameterDeclarationStructure>(arg => ({
                                 kind: TsMorph.StructureKind.Parameter,
                                 name: arg.label,
-                                type: this.buildNativeType(arg.type),
+                                type: this._structTypeBuilder.getNativeType(arg.type.type),
                             })),
                         ],
                         returnType: `DPT.SubmittableExtrinsic`,
@@ -294,7 +274,7 @@ export class AbiTypeBindingProcessor
                         },
                         {
                             name: 'params',
-                            type: this.buildArgsString(constructor.args),
+                            type: this._structTypeBuilder.getArgsString(constructor.args),
                         },
                         {
                             name: 'options',
@@ -306,132 +286,6 @@ export class AbiTypeBindingProcessor
                 })),
             }
         ];
-    }
-    
-    
-    public buildArgsString (args : ContractMetadata.Argument[]) : string
-    {
-        if (args.length === 0) {
-            return 'never[]';
-        }
-        else {
-            const argTypes : string[] = args.map(arg => this.buildNativeType(arg.type));
-            return '[' + argTypes.join(', ') + ']';
-        }
-    }
-    
-    public buildNativeType (typeRef : Partial<ContractMetadata.TypeRef>) : string
-    {
-        const typeDef = this._types[typeRef.type];
-        
-        if (typeDef.kind === 'primitive') {
-            const primitive = (<ContractMetadata.Type.Primitive>typeDef.meta).def.primitive;
-            
-            if (primitive === 'bool') {
-                return 'boolean';
-            }
-            else if ([ 'u8', 'u16', 'u32', 'u64', 'u128', 'i8', 'i16', 'i32', 'i64', 'i128' ].includes(primitive)) {
-                return 'number';
-            }
-            else if ([ 'str' ].includes(primitive)) {
-                return 'string';
-            }
-            else {
-                // todo ld 2022-10-31 09:00:10
-                console.log(typeDef);
-                
-                throw new Exception(
-                    'Unknown type',
-                    1667223448403
-                );
-            }
-        }
-        else if (typeDef.kind === 'variant') {
-            // todo ld 2022-10-31 09:00:02
-            return 'any';
-        }
-        else if (typeDef.kind === 'composite') {
-            const composite = (<ContractMetadata.Type.Composite>typeDef.meta).def.composite;
-            
-            const fields = composite.fields
-                .map(field => field.name + ': ' + this.buildNativeType({ type: field.type }))
-                .join(', ');
-            
-            return '{ ' + fields + ' }';
-        }
-        else if (typeDef.kind === 'array') {
-            const { type, len } = (<ContractMetadata.Type.ArrayType>typeDef.meta).def.array;
-            
-            const innerType = this.buildNativeType({ type });
-            return `DPT.FixedArray<${innerType}, ${len}>`;
-        }
-        else if (typeDef.kind === 'sequence') {
-            const { type } = (<ContractMetadata.Type.Sequence>typeDef.meta).def.sequence;
-            
-            const innerType = this.buildNativeType({ type });
-            return `${innerType}[]`;
-        }
-        else {
-            throw new Exception(
-                'Unknown type',
-                1667028692024
-            );
-        }
-    }
-    
-    public buildCodecType (typeRef : Partial<ContractMetadata.TypeRef>) : string
-    {
-        const typeDef = this._types[typeRef.type];
-        
-        if (typeDef.kind === 'primitive') {
-            const primitive = (<ContractMetadata.Type.Primitive>typeDef.meta).def.primitive;
-            
-            if (primitive === 'bool') {
-                return 'DPT.IBool';
-            }
-            else if ([ 'u8', 'u16', 'u32', 'u64', 'u128', 'i8', 'i16', 'i32', 'i64', 'i128' ].includes(primitive)) {
-                return 'DPT.INumber';
-            }
-            else if ([ 'str' ].includes(primitive)) {
-                return 'DPT.IText';
-            }
-            else {
-                // todo ld 2022-10-31 09:00:10
-                return 'any';
-            }
-        }
-        else if (typeDef.kind === 'variant') {
-            // todo ld 2022-10-31 09:00:10
-            const params = (<ContractMetadata.Type.Variant>typeDef.meta).params;
-            // console.dir((<ContractMetadata.Type.Variant>typeDef.meta), { depth: 10 });
-            
-            return params
-                ?.map(param => this.buildCodecType({ type: param.type }))
-                .filter(type => !!type)
-                .join(' | ');
-        }
-        else if (typeDef.kind === 'composite') {
-            // todo ld 2022-10-31 09:00:10
-            return 'any';
-        }
-        else if (typeDef.kind === 'array') {
-            const { type, len } = (<ContractMetadata.Type.ArrayType>typeDef.meta).def.array;
-            
-            const innerType = this.buildCodecType({ type });
-            return `DPT.IVec<${innerType}>`;
-        }
-        else if (typeDef.kind === 'sequence') {
-            const { type } = (<ContractMetadata.Type.Sequence>typeDef.meta).def.sequence;
-            
-            const innerType = this.buildCodecType({ type });
-            return `DPT.IVec<${innerType}>`;
-        }
-        else {
-            throw new Exception(
-                'Unknown type',
-                1667203276794
-            );
-        }
     }
     
 }
---
+++ src/service/type-binding/StructTypeBuilder.ts
@@ -0,0 +1,228 @@
+import { ContractMetadata } from '@/def';
+import { Exception } from '@/utils/Exception';
+import * as TsMorph from 'ts-morph';
+
+
+type InternalTypeDef = {
+    kind : string,
+    meta : ContractMetadata.TypeDefType,
+};
+
+export type BuiltType = {
+    native : string,
+    codec : string,
+}
+
+
+export class StructTypeBuilder
+{
+    
+    protected _definedTypes : Record<number, InternalTypeDef>;
+    
+    protected _builtTypes : Record<number, BuiltType> = {};
+    protected _typeStatements : Record<string, TsMorph.KindedStructure<any>> = {};
+    
+    protected _newTypeIdx : number = 0;
+    
+    
+    public constructor (types : ContractMetadata.TypeDef[])
+    {
+        this._definedTypes = Object.fromEntries(
+            types.map(typeDef => {
+                return [
+                    typeDef.id,
+                    {
+                        kind: Object.keys(typeDef.type.def)[0],
+                        meta: typeDef.type,
+                    }
+                ];
+            })
+        );
+        
+        for (const [ idx, typeDef ] of Object.entries(this._definedTypes)) {
+            if (!this._builtTypes[idx]) {
+                this.buildType(Number(idx));
+            }
+            
+            console.dir(idx);
+            console.dir(typeDef, { depth: 10 });
+        }
+    }
+    
+    public getNativeType (typeIdx : number) : string
+    {
+        if (!this._builtTypes[typeIdx]) {
+            throw new Exception(
+                'Unknown type',
+                1667305507276
+            );
+        }
+        
+        return this._builtTypes[typeIdx].native;
+    }
+    
+    public getCodecType (typeIdx : number) : string
+    {
+        if (!this._builtTypes[typeIdx]) {
+            throw new Exception(
+                'Unknown type',
+                1667305512350
+            );
+        }
+        
+        return this._builtTypes[typeIdx].codec;
+    }
+    
+    public getTypeStatements () : TsMorph.KindedStructure<any>[]
+    {
+        return Object.values(this._typeStatements);
+    }
+    
+    
+    public getArgsString (args : ContractMetadata.Argument[]) : string
+    {
+        if (args.length === 0) {
+            return 'never[]';
+        }
+        else {
+            const argTypes : string[] = args.map(arg => this.getNativeType(arg.type.type));
+            return '[' + argTypes.join(', ') + ']';
+        }
+    }
+    
+    
+    public buildType (typeIdx : number)
+    {
+        const typeDef = this._definedTypes[typeIdx];
+        
+        if (typeDef.kind === 'primitive') {
+            this._builtTypes[typeIdx] = this.buildPrimitive(<any>typeDef.meta);
+        }
+        else if (typeDef.kind === 'array') {
+            this._builtTypes[typeIdx] = this.buildArray(<any>typeDef.meta);
+        }
+        else if (typeDef.kind === 'sequence') {
+            this._builtTypes[typeIdx] = this.buildSequence(<any>typeDef.meta);
+        }
+        else if (typeDef.kind === 'composite') {
+            this._builtTypes[typeIdx] = this.buildComposite(<any>typeDef.meta);
+        }
+        else if (typeDef.kind === 'variant') {
+            this._builtTypes[typeIdx] = this.buildVariant(<any>typeDef.meta);
+        }
+        else {
+            throw new Exception(
+                `Unknown type ${typeDef.kind}@${typeIdx}`,
+                1667305662214
+            );
+        }
+        
+        return this._builtTypes[typeIdx];
+    }
+    
+    
+    public buildPrimitive (typeDef : ContractMetadata.Type.Primitive) : BuiltType
+    {
+        const { primitive } = typeDef.def;
+        
+        if (primitive === 'bool') {
+            return {
+                native: 'boolean',
+                codec: 'DPT.IBool'
+            };
+        }
+        else if ([ 'u8', 'u16', 'u32', 'u64', 'u128', 'i8', 'i16', 'i32', 'i64', 'i128' ].includes(primitive)) {
+            return {
+                native: 'number',
+                codec: 'DPT.INumber'
+            };
+        }
+        else if ([ 'str' ].includes(primitive)) {
+            return {
+                native: 'string',
+                codec: 'DPT.IText'
+            };
+        }
+        else {
+            // todo ld 2022-10-31 09:00:10
+            throw new Exception(
+                'Unknown type',
+                1667223448403
+            );
+        }
+    }
+    
+    public buildArray (typeDef : ContractMetadata.Type.ArrayType) : BuiltType
+    {
+        const { array: { len, type } } = typeDef.def;
+        const { native, codec } = this.buildType(type);
+        
+        return {
+            native: `DPT.FixedArray<${native}, ${len}>`,
+            codec: `DPT.IVec<${codec}>`,
+        };
+    }
+    
+    public buildSequence (typeDef : ContractMetadata.Type.Sequence) : BuiltType
+    {
+        const { sequence: { type } } = typeDef.def;
+        const { native, codec } = this.buildType(type);
+        
+        return {
+            native: `${native}[]`,
+            codec: `DPT.IVec<${codec}>`,
+        };
+    }
+    
+    public buildComposite (typeDef : ContractMetadata.Type.Composite) : BuiltType
+    {
+        const { path, def: { composite: { fields } } } = typeDef;
+        
+        const name = path.join('$');
+        
+        let declaration : TsMorph.TypeAliasDeclarationStructure;
+        
+        const isSimpleObject = fields.find(field => !field.name) === undefined;
+        if (isSimpleObject) {
+            const strFields = fields
+                .map(field => {
+                    const { native } = this.buildType(field.type);
+                    return field.name + ': ' + native;
+                })
+                .join(', ');
+            
+            declaration = {
+                kind: TsMorph.StructureKind.TypeAlias,
+                name,
+                type: '{ ' + strFields + ' }',
+            };
+        }
+        else {
+            declaration = {
+                kind: TsMorph.StructureKind.TypeAlias,
+                name,
+                type: 'any',
+            };
+        }
+        
+        this._typeStatements[name] = declaration;
+        
+        return {
+            native: name,
+            codec: `DPT.ICompact<${name}>`,
+        };
+    }
+    
+    public buildVariant (typeDef : ContractMetadata.Type.Variant) : BuiltType
+    {
+        const { params } = typeDef;
+        
+        const name = 'Variant' + (++this._newTypeIdx);
+        
+        return {
+            native: `any`,
+            codec: 'any',
+        };
+    }
+    
+}
