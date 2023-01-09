c3dc5bce551724f5778fa96ea0de0c4819eabb4c
l00k <l00k+github@protonmail.com>
2023-01-09 16:17:50 +0100

    fix gitignore issue

--- src/service/project/Initializer.ts
+++ src/service/project/Initializer.ts
@@ -1,5 +1,4 @@
 import { RuntimeContext } from '@/service/project/RuntimeContext';
-import { Exception } from '@/utils/Exception';
 import { Logger } from '@/utils/Logger';
 import chalk from 'chalk';
 import fs from 'fs';
@@ -11,11 +10,11 @@ export class Initializer
     
     protected _logger = new Logger(Initializer.name);
     
-    protected _templates : string[] = [
-        '.gitignore',
-        'accounts.json',
-        'devphase.config.ts',
-    ];
+    protected _templates : Record<string, string> = {
+        'gitignore': '.gitignore',
+        'accounts.json': 'accounts.json',
+        'devphase.config.ts': 'devphase.config.ts',
+    };
     
     protected _directories : string[] = [
         'contracts',
@@ -39,19 +38,19 @@ export class Initializer
         // copy templates
         this._logger.log('Creating files');
         
-        for (const templateFile of this._templates) {
+        for (const [ fromTemplateFile, toTemplateFile ] of Object.entries(this._templates)) {
             const sourceTemplatePath = path.join(
                 this._runtimeContext.paths.devphase,
                 'templates',
-                templateFile
+                fromTemplateFile
             );
             
-            const targetFilePath = `./${templateFile}`;
+            const targetFilePath = `./${toTemplateFile}`;
             if (fs.existsSync(targetFilePath)) {
                 continue;
             }
             
-            this._logger.log(chalk.cyan(templateFile));
+            this._logger.log(chalk.cyan(toTemplateFile));
             
             fs.copyFileSync(
                 sourceTemplatePath,
