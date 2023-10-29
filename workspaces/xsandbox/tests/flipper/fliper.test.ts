853f8d6cbc097f98a56e4cde2a2a88e1838c6ca0
l00k <l00k+github@protonmail.com>
2023-10-30 01:22:59 +0200

    add tx test

--- workspaces/xsandbox/tests/flipper/fliper.test.ts
+++ workspaces/xsandbox/tests/flipper/fliper.test.ts
@@ -1,5 +1,5 @@
 import { Flipper } from '@/typings/Flipper';
-import { ContractType } from '@devphase/service';
+import { ContractType, TxHandler, waitFor } from '@devphase/service';
 import * as PhalaSdk from '@phala/sdk';
 import type { KeyringPair } from '@polkadot/keyring/types';
 
@@ -21,17 +21,47 @@ describe('Flipper', () => {
             { contractType: ContractType.InkCode }
         );
         
-        await factory.deploy();
+        await TxHandler.handle(
+            this.api.tx
+                .phalaPhatContracts.transferToCluster(
+                    100e12,
+                    this.devPhase.mainClusterId,
+                    signer.address
+                ),
+            signer,
+            true
+        );
+        
+        await factory.deploy({ asAccount: signer });
     });
     
     describe('default constructor', () => {
         beforeEach(async function() {
-            contract = await factory.instantiate('default', []);
+            contract = await factory.instantiate('default', [], { asAccount: signer });
         });
         
         it('Should be created with proper intial value', async function() {
             const response = await contract.query.get(signer.address, { cert });
             expect(response.output.toJSON()).to.be.eql({ ok: false });
+        });
+        
+        it('Should be able to flip value', async function() {
+            const tx = contract.tx.flip({
+                gasLimit: 10e12
+            });
+            
+            const result = await TxHandler.handle(
+                tx,
+                signer,
+                true
+            );
+            expect(result.isFinalized).to.be.eql(true);
+            
+            await waitFor(async() => {
+                const response = await contract.query.get(signer.address, { cert });
+                const output = response.output.toJSON();
+                return output.ok;
+            }, 10_000);
         });
     });
     
